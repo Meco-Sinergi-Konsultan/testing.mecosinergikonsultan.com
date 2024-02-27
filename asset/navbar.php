c9e788d81afcf6bc723bbfb3891851c84fed0fbd
Muhammad Syahril <mr.muhammadsyahril@gmail.com>
2024-02-27 23:42:41 +0900

    Update navbar

--- asset/navbar.php
+++ asset/navbar.php
@@ -1,6 +1,6 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
-  <a href="Home" class="navbar-brand p-0">
+  <a href="#" class="navbar-brand p-0">
     <img src="img/logo-msk.png" alt="logo">
   </a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
